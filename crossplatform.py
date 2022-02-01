5cfbcae19cbe06fdfb14e6d334a17770e6604523
jona799t <70392256+jona799t@users.noreply.github.com>
2022-02-01 19:02:47 +0100

    Now with play()

--- crossplatform.py
+++ crossplatform.py
@@ -2,14 +2,22 @@ import sys
 import os
 if os.name == 'nt':
     import msvcrt
+    import winsound
 else:
-    from pynput.keyboard import Key, Listener
+    from pynput.keyboard import Listener
+    from playsound import playsound
 
 def clear():
     os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
 def title(whatToTitle):
     os.system("title " + whatToTitle) if os.name in ('nt', 'dos') else sys.stdout.write(f"\x1b]2;{whatToTitle}\x07")
+
+def play(fileLocation):
+    if os.name == 'nt':
+        winsound.PlaySound(fileLocation, winsound.SND_FILENAME)
+    else:
+        playsound(fileLocation)
 
 def waitForKey(keyToWaitFor):
     if os.name == 'nt':
